f9e24c9ce265e4159225c90a9c6e73fa4e20dcca
mhserrano <mariahcserrano@gmail.com>
2023-08-16 19:14:21 +0100

    Organized the project files and added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container-fluid"></div>
+      <footer>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/mhserrano/weather-react-app"
           target="_blank"
-          rel="noopener noreferrer"
+          rel="noreferrer"
         >
-          Learn React
-        </a>
-      </header>
+          Open-source code
+        </a>{" "}
+        by <strong>Maria Serrano</strong>
+      </footer>
     </div>
   );
 }
-
-export default App;
