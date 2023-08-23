e55832181347085b120acfe8c0c3cd4ea5efb7bb
mhserrano <mariahcserrano@gmail.com>
2023-08-23 21:26:54 +0100

    Added icons component

--- src/CurrentWeather.js
+++ src/CurrentWeather.js
@@ -1,13 +1,11 @@
 import React from "react";
+import WeatherIcons from "./WeatherIcons";
 
 export default function CurrentWeather(props) {
   return (
     <div className="row">
       <div className="col-6 m-auto p-0">
-        <img
-          src="https://help.apple.com/assets/640A52196275DE31D4371B5E/640A52226275DE31D4371B8B/pt_PT/575900edccbc7def167f7874c02aeb0b.png"
-          alt="icon"
-        />
+        <WeatherIcons iconCode={props.data.icon} />
         <span>{props.data.temperature}°</span>
         <button>C</button>
         <button>F</button>
